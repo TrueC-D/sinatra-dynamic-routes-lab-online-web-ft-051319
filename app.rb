5040ab30b220004f432afd5184bc3ea9a502b944
Alethea Cary <aletheacary@gmail.com>
2019-07-04 21:50:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -25,21 +25,8 @@ class App < Sinatra::Base
   end
   
   get '/:operation/:number1/:number2' do
-    
-  #   @operation_word = params[:operation]
-  #   @operation = nil
-  #   case @operation_word
-  #     when "add"
-  #       @operation = +
-  #     when "subtract"
-  #       @operation = -
-  #     when "multiply"
-  #       @operation = *
-  #     when "divide"
-  #       @operation = /
-  #     else
-  #       @operation = nil
-  #   end
+  
+  #   @operation = params[:operation]
   #   @num1 = params[:number1].to_i
   #   @num2 = params[:number2].to_i
   #   @result = @num1 @operation @num2
