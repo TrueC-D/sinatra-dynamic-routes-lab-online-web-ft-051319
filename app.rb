deb52c56b7801f48b1aa2f796451d5d8e090be59
Alethea Cary <aletheacary@gmail.com>
2019-07-04 21:43:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,9 +16,9 @@ class App < Sinatra::Base
     @number = params[:number].to_i
     @phrase = params[:phrase].gsub("%20", " ")
     count = 0
-    if count < @number do 
+    while count <= @number do 
       count+=1
-      "#{@phrase}"
+      @phrase.to_s
     end
   end
   
