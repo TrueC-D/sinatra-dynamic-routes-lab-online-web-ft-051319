d198c1f43880c72274bc21de1db3aaebf6d5f784
Alethea Cary <aletheacary@gmail.com>
2019-07-04 21:51:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -29,7 +29,7 @@ class App < Sinatra::Base
   #   @operation = params[:operation]
   #   @num1 = params[:number1].to_i
   #   @num2 = params[:number2].to_i
-  #   @result = @num1 @operation @num2
+  #   @result @num1.send(@operation, @num2)
   #   @result
   end
 end
